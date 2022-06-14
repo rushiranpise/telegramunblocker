a866538e4cd903cb46de50b4e26afbbcd31a2fb7
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-06-14 15:33:21 +0530

    update description

--- telegramunblocker.js
+++ telegramunblocker.js
@@ -1,15 +1,14 @@
 // ==UserScript==
+// @id               telegramunblocker@https://github.com/NRSA-devs/telegramunblocker
 // @name             Unblock Telegram Links
-// @namespace        rushiranpise
+// @namespace        https://github.com/NRSA-devs/telegramunblocker
 // @version          0.1
 // @description      Redirect t.me to telegram.me
-// @author           rushiranpise //NRSA-devs
+// @author           rushiranpise - NRSA-devs
 // @include          https://t.me/*
 // @include          http://t.me/*
 // @grant             none
-// ==/UserScript==
 // @run-at         document-start
-// @run-at         document-end
-// @run-at         document-idle
+// ==/UserScript==
 
 document.location = document.URL.replace('t.me','telegram.me');
