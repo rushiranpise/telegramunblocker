ad5ffb407fcce44deb15eb78ed42e816a107cdbd
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-06-14 15:49:50 +0530

    update code
    @match>>@include : @include is deprecated , 
    @name must be defined first
    @run-at all

--- telegramunblocker.js
+++ telegramunblocker.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @id               telegramunblocker@https://github.com/NRSA-devs/telegramunblocker
 // @name             Unblock Telegram Links
 // @namespace        https://github.com/NRSA-devs/telegramunblocker
 // @version          0.1
+// @id               telegramunblocker@https://github.com/NRSA-devs/telegramunblocker
 // @description      Redirect t.me to telegram.me
 // @author           rushiranpise - NRSA-devs
-// @include          https://t.me/*
-// @include          http://t.me/*
+// @match            *://t.me/*
 // @grant             none
 // @run-at         document-start
+// @run-at         document-end
+// @run-at         document-idle
 // ==/UserScript==
 
 document.location = document.URL.replace('t.me','telegram.me');
